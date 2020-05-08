f6b900fd446ff0c8a6163f526064c9c175fa7adf
Paul <pgurnig.development@gmail.com>
2020-05-08 08:20:00 -0500

    Adding installation-review.sh

---
+++ installation-review.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# check to see if git is installed
+git --version
+
+# check to see if java is installed
+java -version
+
+# does the Jenkins directory exist
+ls -l /var/lib/jenkins
+
+# is Maven installed
+mvn -v
+
+# is Jenkins running
+ps ax | grep Jenkins
+
+# manual step - auto apply updates
+sudo vi /etc/yum/yum-cron.conf
+
+# check the cron log
+sudo cat /var/log/yum.log
